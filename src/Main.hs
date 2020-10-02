bc8663f3cc4189a96b97b5aea57d75ddc7e1a707
~~~ <flutterlice@gmail.com>
2020-10-02 21:40:32 +0500

    Fix of a bug caused by underconsuming of the input stream

--- src/Main.hs
+++ src/Main.hs
@@ -6,35 +6,33 @@ import qualified Data.Text.IO as Text
 import Data.Functor
 import Data.Maybe
 
-data WCCToken = WCCUnidentified
-              | WCCFunc
-              | WCCWhile
-              | WCCLocal
-              | WCCNumeric Int
-              | WCCNewline
-              | WCCMinus
-              | WCCPlus
-              | WCCGreater
-              | WCCLess
-              | WCCEquals
-              | WCCOCurlyBracket
-              | WCCCCurlyBracket
-              | WCCOBracket
-              | WCCCBracket
-              | WCCColon
-              | WCCEOF
-              | WCCMinusEquals
-              | WCCPlusEquals
-              | WCCToken String deriving (Show, Eq)
+data Token = Func
+              | While
+              | Local
+              | Numeric Int
+              | Newline
+              | Minus
+              | Plus
+              | Greater
+              | Less
+              | Equals
+              | OCurlyBracket
+              | CCurlyBracket
+              | OBracket
+              | CBracket
+              | Colon
+              | EOF
+              | MinusEquals
+              | PlusEquals
+              | Symbol String deriving (Show, Eq)
 
-data WCCType = WCC_I32
-             | WCC_Undefined
+data Type = TypeI32
  deriving Show
 
 newtype VarName = VarName String
  deriving Show
 
-data TypedVariable = TypedVariable WCCType VarName 
+data TypedVariable = TypedVariable Type VarName 
  deriving Show
 
 data Value = VInt Int
@@ -56,82 +54,79 @@ data Statement = STLocalVarDef TypedVariable (Maybe Expression)
 data Block = Block [Statement]
  deriving Show
 
-data WCCFuncDef = WCCFuncDef {
+data FuncDef = FuncDef {
     _wccfdName :: String,
     _wccfdArgs :: [TypedVariable],
-    _wccfdReturnType :: WCCType,
+    _wccfdReturnType :: Type,
     _wccfdBody :: Block
   } deriving Show
 
-parseFile :: GenParser Char st [WCCToken]
+parseFile :: GenParser Char st [Token]
 parseFile = do
   (flip manyTill) eof $ do
     skipMany $ oneOf " \t"
     attemptDigit 
       <|> (parseString <$> attemptString)
       <|> attemptSymbol
-      <|> eof $> WCCEOF
-      <|> pure WCCUnidentified
+      <|> eof $> EOF
+      <|> fail "Couldnt parse any recognized token"
  where attemptDigit = do
-         WCCNumeric <$> read <$> many1 digit
+         Numeric <$> read <$> many1 digit
        attemptString = do
          many1 alphaNum
        attemptSymbol = do
-         parseBiChar <$> anyChar <*> (lookAhead anyChar <|> pure '\0')
-       -- BiChar
-       parseBiChar '\n'  _  = WCCNewline
-       parseBiChar '-'  '=' = WCCMinusEquals
-       parseBiChar '-'   _  = WCCMinus
-       parseBiChar '+'  '=' = WCCPlusEquals
-       parseBiChar '+'   _  = WCCPlus
-       parseBiChar '>'   _  = WCCGreater
-       parseBiChar '<'   _  = WCCLess
-       parseBiChar '='   _  = WCCEquals
-       parseBiChar '{'   _  = WCCOCurlyBracket
-       parseBiChar '}'   _  = WCCCCurlyBracket
-       parseBiChar '('   _  = WCCOBracket
-       parseBiChar ')'   _  = WCCCBracket
-       parseBiChar ':'   _  = WCCColon
-       parseBiChar  _    _  = WCCUnidentified
+            try (string "\n") $> Newline
+        <|> try (string "-=") $> MinusEquals
+        <|> try (string "-" ) $> Minus
+        <|> try (string "+=") $> PlusEquals
+        <|> try (string "+" ) $> Plus
+        <|> try (string ">" ) $> Greater
+        <|> try (string "<" ) $> Less
+        <|> try (string "=" ) $> Equals
+        <|> try (string "{" ) $> OCurlyBracket
+        <|> try (string "}" ) $> CCurlyBracket
+        <|> try (string "(" ) $> OBracket
+        <|> try (string ")" ) $> CBracket
+        <|> try (string ":" ) $> Colon
        -- String
-       parseString "while"  = WCCWhile
-       parseString "func"   = WCCFunc
-       parseString "local"  = WCCLocal
-       parseString s        = WCCToken s
+       parseString "while"  = While
+       parseString "func"   = Func
+       parseString "local"  = Local
+       parseString s        = Symbol s
 
-data T = P WCCToken | F WCCFuncDef deriving Show
+data T = P Token | F FuncDef deriving Show
 
 tok p = tokenPrim show update_pos get where
   get a = if p == a then Just (P p) else Nothing
 
-update_pos :: SourcePos -> WCCToken -> [WCCToken] -> SourcePos
+update_pos :: SourcePos -> Token -> [Token] -> SourcePos
 update_pos pos _ (tok:_) = setSourceLine (incSourceColumn pos 1) 0
 update_pos pos _ [] = pos
 
 gtok = tokenPrim show update_pos get where
-  get (WCCToken a) = Just a
+  get (Symbol a) = Just a
   get _ = Nothing
 
-parseType :: String -> Maybe WCCType 
-parseType "i32" = Just WCC_I32
+parseType :: String -> Maybe Type 
+parseType "i32" = Just TypeI32
 parseType _ = Nothing
 
-parseTokens :: GenParser WCCToken st [T]
+parseTokens :: GenParser Token st [T]
 parseTokens = do 
   many $ do
-    tok WCCFunc        <?> "func token"
+    tok Func        <?> "func token"
     nam <- gtok        <?> "func name after `func` keyword"
-    tok WCCOBracket    <?> "opening bracket after func name"
+    tok OBracket    <?> "opening bracket after func name"
     vars <- many $ do
       argname <- gtok  <?> "argument name"
-      tok WCCColon     <?> "colon after argument name"
+      tok Colon     <?> "colon after argument name"
       argType <- fromJust <$> parseType <$> gtok 
                        <?> "argument type after colon"
       pure $ TypedVariable argType $ VarName argname
-    tok WCCCBracket    <?> "closing bracket after argument list"
-    tok WCCColon       <?> "colon after argument list"
+    tok CBracket    <?> "closing bracket after argument list"
+    tok Colon       <?> "colon after argument list"
     retType <- fromJust <$> parseType <$> gtok
-    pure $ F $ WCCFuncDef nam vars retType (Block [])
+    pure $ F $ FuncDef nam vars retType (Block [])
 
 main = do
   res <- parse parseFile "" <$> Text.unpack <$> Text.readFile "fib.wc"
