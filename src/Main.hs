9e26b53e406058903e0bd7e0211f8b934d58badf
~~~ <flutterlice@gmail.com>
2020-10-03 17:35:21 +0500

    Full sample file parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import Text.ParserCombinators.Parsec
+import Text.Parsec (ParsecT)
 import qualified Data.Text    as Text 
 import qualified Data.Text.IO as Text 
 import Data.Functor
 import Data.Maybe
+import Control.Monad.Identity
 
 data Token = Func
               | While
@@ -106,9 +108,18 @@ gtok = tokenPrim show update_pos get where
   get (Symbol a) = Just a
   get _ = Nothing
 
-parseType :: String -> Maybe Type 
-parseType "i32" = Just TypeI32
-parseType _ = Nothing
+ntok = tokenPrim show update_pos get where
+  get (Numeric a) = Just a
+  get _ = Nothing
+
+parseType :: String -> Type
+parseType "i32" = TypeI32
+parseType _ = error "TypeParsingErrorStub" -- FIXME: please
+
+allowNewLine :: ParsecT [Token] u Identity FuncDef
+allowNewLine = do
+  many $ tok Newline
+  pure $ DummyToken Newline
 
 parseTokens :: GenParser Token st [FuncDef]
 parseTokens = do 
@@ -116,18 +127,88 @@ parseTokens = do
     tok Func          <?> "func token"
     nam <- gtok       <?> "func name after `func` keyword"
     tok OBracket      <?> "opening bracket after func name"
-    vars <- many $ do
+    vars <- (many $ do
       argname <- gtok <?> "argument name"
       tok Colon       <?> "colon after argument name"
-      argType <- fromJust <$> parseType <$> gtok 
+      argType <- parseType <$> gtok
                       <?> "argument type after colon"
-      pure $ TypedVariable argType $ VarName argname
+      pure $ TypedVariable argType $ VarName argname)
+                      <?> "argument list"
     tok CBracket      <?> "closing bracket after argument list"
     tok Colon         <?> "colon after argument list"
-    retType <- fromJust <$> parseType <$> gtok
-    between (tok OCurlyBracket) (tok CCurlyBracket) $ do 
-      pure ()
-    pure $ FuncDef nam vars retType (Block [])
+    retType <- parseType <$> gtok
+    statements <- between (tok OCurlyBracket) (tok CCurlyBracket) $ do
+      many parseStatement
+    pure $ FuncDef nam vars retType (Block statements)
+
+parseStatement = do
+  allowNewLine
+  try $ tryParseLocalVarDef
+  <|> tryParseWhileLoop
+  <|> try tryParseAssignment
+  <|> try tryParseSubstractAssignment
+  <|> tryParseSTExpression
+
+tryParseLocalVarDef = do
+  tok Local
+  varname <- gtok
+  tok Colon
+  typename <- parseType <$> gtok
+  expr <- try (do
+    tok Equals
+    Just <$> tryParseExpression) <|> pure Nothing
+  tok Newline
+  pure $ STLocalVarDef (TypedVariable typename (VarName varname)) expr
+
+tryParseWhileLoop = do
+  tok While
+  tok OBracket
+  expr <- tryParseExpression
+  tok CBracket
+  statements <- (between (tok OCurlyBracket) (tok CCurlyBracket) $ many parseStatement)
+  tok Newline
+  pure $ STWhileLoop expr $ Block statements
+
+tryParseAssignment = do
+  destname <- gtok
+  tok Equals
+  expr <- tryParseExpression
+  tok Newline
+  pure $ STAssignment (VarName destname) expr
+
+tryParseSubstractAssignment = do
+  destname <- gtok
+  tok MinusEquals
+  expr <- tryParseExpression
+  tok Newline
+  pure $ STSubtractAssignment (VarName destname) expr
+
+tryParseSTExpression = do
+  expr <- tryParseExpression
+  tok Newline
+  pure $ STExpression expr
+
+tryParseExpression = do
+  left <- tryParseValue <|> tryParseVariable
+  tryParseSum left <|> tryParseComparisson left <|> pure left
+
+tryParseSum left = do
+  tok Plus
+  expr <- tryParseExpression
+  pure $ EXSum left expr
+
+tryParseComparisson left = do
+  tok Greater
+  expr <- tryParseExpression
+  pure $ EXSum left expr
+
+tryParseValue = do
+  number <- ntok
+  pure $ EXValue $ VInt number
+
+tryParseVariable = do
+  varname <- gtok
+  pure $ EXVariable (VarName varname)
 
 main = do
   res <- parse parseFile "" <$> Text.unpack <$> Text.readFile "fib.wc"
